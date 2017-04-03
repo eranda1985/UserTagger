c3c5afa92f746c05dd41e2131e0010cbdd75db2c
galhenel <galhenel@ITU258887>
2017-04-03 14:21:36 +0930

    Added UsertaggerValidator

---
+++ StudentTagApplication/Core/Validators/UsertaggerValidator.cs
@@ -0,0 +1,17 @@
+﻿using Api.Core.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniSA.UserTagger.Core.Validators
+{
+    public static class UserTaggerValidator
+    {
+         public static Validation CheckNull<T>(T obj, string param) where T: class
+        {
+            return Validate.Begin().IsNotNull<T>(obj, param).Check();
+        }
+    }
+}
