d42a1e206b2a3974aa1bb09f7e4ed3d975c145d4
galhenel <eranda.galhenage@unisa.edu.au>
2017-04-04 13:26:04 +0930

    Added Unit Test for a MockTagHandler

--- StudentTagApplication/Handlers/ScholarshipTagHandler.cs
+++ StudentTagApplication/Handlers/ScholarshipTagHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using UniSA.UserTagger.ApiClientWorker.Interfaces;
 using UniSA.UserTagger.Core.DTO;
 using UniSA.UserTagger.Core.Events;
--- StudentTagApplicationTests/Handlers/MockTagHandlerTest.cs
+++ StudentTagApplicationTests/Handlers/MockTagHandlerTest.cs
@@ -1,28 +1,99 @@
-﻿using System;
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using UniSA.UserTagger.ApiClientWorker.Interfaces;
+using UniSA.UserTagger.Core.Components;
 using UniSA.UserTagger.Core.DTO;
 using UniSA.UserTagger.Core.Events;
 using UniSA.UserTagger.Core.Interfaces;
-using UniSA.UserTagger.Core.Logger;
 using UniSA.UserTagger.Core.Models;
 using UniSA.UserTagger.Core.Repository;
+using UniSA.UserTagger.Handlers;
 
-namespace UniSA.UserTagger.Handlers
+namespace StudentTagApplicationTests.Handlers
 {
-    public class AssignmentTagHandler : BaseHandler, ISubscriber<TagUpdateEvent>
+    [TestFixture]
+    public class MockTagHandlerTest
     {
-        private Logger _logger;
+        private IUrbanAirshipClientWorker _worker;
+        private IConverter<TagDTO, TagModel> _tagDTOConverter;
+        private MockTagHandler _mockTagHandler;
+        private TagUpdateEvent _event;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _worker = Substitute.For<IUrbanAirshipClientWorker>();
+            _tagDTOConverter = Substitute.For<IConverter<TagDTO, TagModel>>();
+            _mockTagHandler = new MockTagHandler("tagName", _worker, _tagDTOConverter);
+
+            var eventAggtor = new EventAggregator();
+            eventAggtor.RegisterEvent<TagUpdateEvent>();
+            _event = eventAggtor.RegisteredTagUpdateEvent;
+            _event.Initialize(eventAggtor);
+            _mockTagHandler.Subscribe(_event);
+
+        }
+
+        [Test]
+        public void Should_Call_Worker_ProcessTagAdd_Successfully()
+        {
+            TagDTO dto = new TagDTO
+            {
+                IsNew =true,
+                IsInstall =true,
+                Name = "tagName",
+                TagGroup = new List<TagGroupDTO>
+                {
+                    new TagGroupDTO
+                    {
+                        Id = 1,
+                        Name = "tag_group_1"
+                    }
+                }
+            };
+
+            _event.Publish(dto);
+            _worker.Received().ProcessTagAdd(Arg.Any<TagStructureDTO>());
+        }
+
+        [Test]
+        public void Should_Call_Worker_ProcessTagRemove_Successfully()
+        {
+            TagDTO dto = new TagDTO
+            {
+                IsNew = true,
+                IsInstall = false,
+                Name = "tagName",
+                TagGroup = new List<TagGroupDTO>
+                {
+                    new TagGroupDTO
+                    {
+                        Id = 1,
+                        Name = "tag_group_1"
+                    }
+                }
+            };
+
+            _event.Publish(dto);
+            _worker.Received().ProcessTagRemove(Arg.Any<TagDTO>());
+        }
+    }
+
+    public class MockTagHandler : BaseHandler, ISubscriber<TagUpdateEvent>
+    {
         private IUrbanAirshipClientWorker _worker;
         private IConverter<TagDTO, TagModel> _tagDTOConverter;
 
-        public AssignmentTagHandler(
+        public MockTagHandler(
             string tagName,
             IUrbanAirshipClientWorker worker,
             IConverter<TagDTO, TagModel> tagDTOConverter) : base(tagName)
         {
-            _logger = new Logger(GetType());
             _worker = worker;
             _tagDTOConverter = tagDTOConverter;
         }
@@ -33,18 +104,16 @@ namespace UniSA.UserTagger.Handlers
             {
                 if (tagName == incomingTag.Name)
                 {
-                    // Do stuff related to this tag
-                    _logger.Debug(string.Format("Entering {0} tag handler logic", tagName));
-
                     if (incomingTag.IsNew && incomingTag.IsInstall)
                     {
-                        // Add tag logic
+                        #region Logic for Add tag
                         TagStructureDTO source = new TagStructureDTO();
 
                         // Get the relevant Uids
                         using (var repo = new ScholarshipUserRepository())
                         {
-                            var result = repo.List("select u.UId from ScholarshipUserDetail u where u.FirstName = 'Eranda'");
+                            var result = repo.List("select u.UId from ScholarshipUserDetail u");
+
                             source.UidList.AddRange(result.Select(x => x.UserId));
                         }
 
@@ -57,6 +126,8 @@ namespace UniSA.UserTagger.Handlers
                         // Call the worker to process payload. 
                         var res = _worker.ProcessTagAdd(source);
 
+                        if (res == null) return;
+
                         if (res.IsActionCompleted && res.IsSuccess && res.OriginalAPIResponse != null)
                         {
                             //Update the tag status in tag registry 
@@ -69,12 +140,16 @@ namespace UniSA.UserTagger.Handlers
                                 repo.Update(entry);
                             }
                         }
+                        #endregion
                     }
 
                     else if (incomingTag.IsNew && (!incomingTag.IsInstall))
                     {
+                        #region Logic for Remove tag
                         // Logic for Remove tag
                         var res = _worker.ProcessTagRemove(incomingTag);
+
+                        if (res == null) return;
 
                         if (res.IsActionCompleted && res.IsSuccess && res.OriginalAPIResponse != null)
                         {
@@ -88,6 +163,7 @@ namespace UniSA.UserTagger.Handlers
                                 repo.Update(entry);
                             }
                         }
+                        #endregion
                     }
                 }
             });
