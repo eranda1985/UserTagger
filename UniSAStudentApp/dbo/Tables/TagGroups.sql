4ab7feee57366e67afa22013c1b994e5a7a7aaea
galhenel <eranda.galhenage@unisa.edu.au>
2017-04-07 10:42:14 +0930

    Database Project updated.

---
+++ UniSAStudentApp/dbo/Tables/TagGroups.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[TagGroups] (
+    [Id]   INT           NOT NULL,
+    [Name] VARCHAR (250) NULL,
+    CONSTRAINT [PK_TagGroups] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
--- UniSAStudentApp/dbo/Tables/Tags.sql
+++ UniSAStudentApp/dbo/Tables/Tags.sql
@@ -1,8 +1,16 @@
 ﻿CREATE TABLE [dbo].[Tags] (
-    [Id]        INT           IDENTITY (1, 1) NOT NULL,
-    [Name]      VARCHAR (250) NULL,
-    [ToInstall] BIT           NULL,
-    [IsNew]     BIT           NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC) 
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [Name]         VARCHAR (250) NOT NULL,
+    [Description]  VARCHAR (250) NULL,
+    [ToInstall]    BIT           DEFAULT ((1)) NULL,
+    [IsActivated]  BIT           DEFAULT ((0)) NULL,
+    [TagGroup]     INT           DEFAULT ((1)) NULL,
+    [CreatedDate]  DATETIME      DEFAULT (getdate()) NULL,
+    [ModifiedDate] DATETIME      NULL,
+    [IsDeleted]    BIT           DEFAULT ((0)) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Tags_To_TagGroups] FOREIGN KEY ([TagGroup]) REFERENCES [dbo].[TagGroups] ([Id]) ON DELETE CASCADE
 );
+
+
 
